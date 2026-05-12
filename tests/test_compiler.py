cf4032e620e634e3006482f70d3ab3f542c0fba6
LanderOtto <alberto.mulone@unito.it>
2026-05-12 15:08:00 +0200

    BREAKING CHANGE: Added the `outdir` parameter to the `compiler` CLI. This breaks existing subclasses of the `BaseCompiler` class.

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,23 +1,28 @@
+from __future__ import annotations
+import contextlib
 import pathlib
-
+import tempfile
 
 from swirlc.main import main
 
 EXAMPLES_PATH = pathlib.Path(__file__).parent.parent / "examples"
 
 
-def _compile(trace_file: str, config_file: str) -> None:
-    assert main(["compile", trace_file, config_file]) == 0
+def _compile(trace_file: str, config_file: str, outdir: str | None = None) -> None:
+    with contextlib.ExitStack() as stack:
+        if outdir is None:
+            outdir = stack.enter_context(tempfile.TemporaryDirectory())
+        assert main(["compile", trace_file, config_file, "--outdir", outdir]) == 0
 
 
-def test_example1():
+def test_example1() -> None:
     _compile(
         str(EXAMPLES_PATH / "example1" / "example1.swirl"),
         str(EXAMPLES_PATH / "example1" / "config.yml"),
     )
 
 
-def test_example2():
+def test_example2() -> None:
     _compile(
         str(EXAMPLES_PATH / "example2" / "example2.swirl"),
         str(EXAMPLES_PATH / "example2" / "config.yml"),
